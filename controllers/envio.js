3edd6bd7685a637982a764b937ff2406f19dd929
JuanDi <juandixarteagaperez210@gmail.com>
2023-12-05 10:19:37 -0500

    first commit

---
+++ controllers/envio.js
@@ -0,0 +1,66 @@
+const {response} = require('express')
+
+const Envio = require('../models/envio')
+
+const getEnvio = async(req, res) => {
+
+    const envios = await Envio.find(); //Obtener todos los documentos de una colección
+    res.json({
+        msg: envios
+    })
+}
+
+const postEnvio = async(req, res) => {
+    const datos = req.body //Capturar datos de la url-postman
+    let mensaje = 'Inserción exitosa'
+    try {
+        const envio = new Envio(datos)
+        console.log(envio) //Instanciar el objeto
+        await envio.save() //Guardar en la base de dato
+        console.log(envio)
+    } catch (error) {
+        mensaje = error
+        console.log(error)
+    }
+    res.json({
+        msg: mensaje
+    })
+}
+
+const putEnvio = async(req, res) => {
+    const {_id, registro_envio, nombre, apellido, email, telefono, direccion, ciudad, postal, pais, departamento } = req.body //Desesctructurar
+    console.log(req.body)
+    let mensaje = ''
+    try {
+        const envio = await Envio.findOneAndUpdate({_id: _id},
+        {registro_envio: registro_envio, nombre: nombre, apellido:apellido, email:email, telefono:telefono, direccion: direccion, ciudad: ciudad, postal:postal, pais: pais, departamento: departamento})
+        mensaje = 'Actualización exitosa'
+    } catch (error) {
+        mensaje = error
+    }   
+    res.json({
+        msg:mensaje
+    })
+}
+
+const deleteEnvio = async(req, res) => {
+    const { registro_envio} = req.query //Desesctructurar
+    let mensaje = ''
+    try {
+        const envio = await Envio.findOneAndDelete({registro_envio: registro_envio})
+        mensaje = 'Eliminación exitosa'
+    } catch (error) {
+        mensaje = error
+    }   
+    res.json({
+        msg:mensaje
+    })
+}
+
+
+module.exports = {
+    getEnvio,
+    postEnvio,
+    putEnvio,
+    deleteEnvio
+}
---
+++ models/envio.js
@@ -0,0 +1,64 @@
+const {Schema, model } = require('mongoose')
+
+
+const EnvioSchema = ({
+
+    registro_envio:{
+        // type: Number,
+        // unique:true,
+        // AutoIncrement:true
+        type: String,
+        required: true,
+        default: () => nanoid(7),
+        index: { unique: true },
+        
+    },
+
+    nombre:{
+        type: String,
+        required:[true, 'El nombre de usuario es requirido']
+    },
+
+    apellido:{
+        type: String,
+        required:[true, 'El apellido es requirido']
+    },
+
+    email:{
+        type: String,
+        required:[true, 'El correo es requirido']
+    },
+
+    telefono:{
+        type: Number,
+        required:[true, 'El telefono es requirido']
+    },
+
+    direccion:{
+        type: String,
+        required:[true, 'La direccion es requirido']
+    },
+
+    ciudad:{
+        type: String,
+        required:[true, 'La ciudad es requirido']
+    },
+
+    postal:{
+        type: String,
+        required:[true, 'El codigo postal es requirido']
+    },
+
+    pais:{
+        type: String,
+        required:[true, 'El pais es requirido']
+    },
+
+    departamento:{
+        type: String,
+        required:[true, 'El departamento es requirido']
+    },
+})
+
+
+module.exports = model('Envio', EnvioSchema)
